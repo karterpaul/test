af7c1f78a6a8a82e1db02155089f586a2840fc31
karterpaul <karterpauls@gmail.com>
2017-02-16 09:53:10 +0530

    cleared the unit test

--- tests/MyClassTest.php
+++ tests/MyClassTest.php
@@ -6,7 +6,7 @@ class MyClassTest extends PHPUnit_Framework_TestCase {
 
     public function testDemo() {
         $x = new MyClass();
-        $this->assertEquals(1, $x->demo(3));
+        $this->assertEquals(1, $x->demo(1));
     }
 }
 
